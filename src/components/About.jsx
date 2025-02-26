d6b464df74b8fc64864e3faad0d0ec53f9000a4d
AzaanTechzilla <azaan.khalid@techzillaglobal.com>
2025-02-26 16:58:45 +0500

    testing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={fadeIn("right", "spring", 0.1 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card "
+      >
+<div options={{max: 45, scale: 1,speed: 450 }} className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+<img src={icon} alt={title} className="w-16 h-16 object-contain" />
+<h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+</div>
+
+      </motion.div>
+    </Tilt>
+  )
+};
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Intoduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+        voluptas natus, neque, tenetur aut, eius at vel facere velit cumque
+        molestias ipsam quaerat dolores. Dicta vitae nihil rerum quasi
+        recusandae odit quibusdam obcaecati dolores adipisci reprehenderit quo
+        unde fuga quidem incidunt distinctio dolore, itaque ea consequuntur,
+        voluptatum nostrum at. Suscipit.
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,7 +2,7 @@ import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import Loader from "../Loader";
-const Computers = () => {
+const Computers = ({isMobile}) => {
   const computer = useGLTF("/desktop_pc/scene.gltf");
   return (
     <>
@@ -20,8 +20,8 @@ const Computers = () => {
         />
         <primitive
           object={computer.scene}
-          scale={0.75}
-          position={[0, -3.25, -1.5]}
+          scale={isMobile ? 0.7: 0.75}
+          position={isMobile ?[ 0, -3,-1] :[0, -3.25, -1.5]}
           rotation={[-0.01, -0.2, -0.1]}
         />
       </mesh>
@@ -29,11 +29,23 @@ const Computers = () => {
   );
 };
 const ComputersCanvas = () => {
+  const [isMobile , setIsMobile] = useState(false);
+  useEffect(() => {
+   const mediaQuery = window.matchMedia('(max-width: 500px)');
+   setIsMobile(mediaQuery.matches);
+   const handleMediaQueryChange = (e)=>{
+setIsMobile(e.matches)
+   }
+   mediaQuery.addEventListener('change', handleMediaQueryChange);
+    return ()=>{
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, []);
   return (
     <Canvas
       frameloop="demand"
       shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [20, 3, 5], fov: isMobile ? 60 : 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<Loader />}>
@@ -42,7 +54,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
